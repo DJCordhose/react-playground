e34a3ba23a275e41c806037929835fb64713bd8a
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-06-17 12:57:19 +0200

    preparation for enterjs talk

--- code/fluxible-isomorphic/components/Html.js
+++ code/fluxible-isomorphic/components/Html.js
@@ -8,7 +8,6 @@ export default class Html extends React.Component {
                 <meta charSet="utf-8" />
                 <title>Fluxible Isomorphic Example</title>
                 <meta name="viewport" content="width=device-width, user-scalable=no" />
-                <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css" />
             </head>
             <body>
                 <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
